9df5c0a9c1ee84744aeaccd3e8726b738f30e95a
Darsi Gangothri <77739656+darsigangothri06@users.noreply.github.com>
2021-06-15 16:45:48 +0530

    Find min, max of an array and thier reverse sum
    C Program to find minimum and maximum of an array and calculating their reverse sum

---
+++ minmaxarray.c
@@ -0,0 +1,54 @@
+//finding min and max of an array and thier sum and reverse sum
+#include <stdio.h>
+int rev(int);
+int main()
+{
+    int a[20], min, max, n, i, orgsum, sum;
+    printf("Enter the size of the array(<=20): ");
+    scanf("%d", &n);
+    printf("Enter the elements of the array: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    min = max = a[0];
+    for(i = 0; i<n; i++)
+    {
+        if (max<a[i])
+            max = a[i];
+    }
+     for(i = 0; i<n; i++)
+    {
+        if (min>a[i])
+            min = a[i];
+    }
+
+    printf("Min is %d and Max is %d", min, max);
+
+    orgsum = min+max;
+
+    printf("\nOrg sum is %d", orgsum);
+
+    if(orgsum>10)
+        sum = rev(orgsum);
+    else
+        sum = orgsum;
+
+    printf("\nREVSUM is %d", sum);
+    return 0;
+}
+
+int rev(int a)
+{
+    int num=a, revr, rem;
+
+    while(num>0)
+    {
+        rem = num%10;
+        revr = revr*10 + rem;
+        num /= 10;
+    }
+    return(revr);
+
+}
+
+
+
